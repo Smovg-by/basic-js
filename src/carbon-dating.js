3a79394ca8636fcfb0bb54d5f2ca8cdee671c878
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-09 22:19:21 +0300

    feat: code upddate

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,18 +4,23 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity == "object") {
+    return false;
+  }
+  if (typeof sampleActivity == "undefined") {
+    return false;
+  }
+  sampleActivity = +sampleActivity;
+  if (sampleActivity < 15) return false;
   const MODERN_ACTIVITY = 15;
   const HALF_LIFE_PERIOD = 5730;
-  let k, t;
-
-  k = 0.693 / HALF_LIFE_PERIOD;
+  let t;
 
   t = Math.ceil(
     Math.log(sampleActivity / MODERN_ACTIVITY) / (0.693 / HALF_LIFE_PERIOD)
   );
   if (isNaN(t) == true) {
     return false;
-  } else {
-    return t;
   }
+  return t;
 };
