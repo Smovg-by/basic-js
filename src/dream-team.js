dcbfac4161a2adbada921866f09b75f78c66f558
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-10 21:12:58 +0300

    fix: test issue fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,18 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  // не проходит два теста
-
   let arr = [];
 
   let noSpaces = members.map((item) => item.trim());
 
   for (i = 0; i < noSpaces.length; i++) {
     if (typeof noSpaces[i] == "string") {
-      arr.push(noSpaces[i][0].trim());
+      arr.push(noSpaces[i][0].split(" ").join(""));
     } else {
       continue;
     }
   }
   return arr.sort().join("").toUpperCase();
 };
+
+//
