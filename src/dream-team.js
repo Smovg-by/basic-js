64274470b94e4510df2abd0c90aeeffd7ba688f6
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-10 21:24:21 +0300

    fix: test issue fix

--- src/dream-team.js
+++ src/dream-team.js
@@ -3,11 +3,15 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function createDreamTeam(members) {
   let arr = [];
 
+  if (members == null || members == undefined || members.length == undefined) {
+    return false;
+  }
+
   let noSpaces = members.map((item) => item.trim());
 
   for (i = 0; i < noSpaces.length; i++) {
     if (typeof noSpaces[i] == "string") {
-      arr.push(noSpaces[i][0].split(" ").join(""));
+      arr.push(noSpaces[i][0]);
     } else {
       continue;
     }
@@ -15,4 +19,6 @@ module.exports = function createDreamTeam(members) {
   return arr.sort().join("").toUpperCase();
 };
 
-//
+// 1) should return false on wrong type
+// 2) should ignore non-string values
+// 3) should handle non-standard cases
