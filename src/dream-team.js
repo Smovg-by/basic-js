b6ca4279512ae263124bb3d8fa13af76338c5b80
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-10 21:04:32 +0300

    fix: fix test issue

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,13 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  let noSpaces = members.map((item) => item.trim());
+  // не проходит два теста
 
   let arr = [];
 
+  let noSpaces = members.map((item) => item.trim());
+
   for (i = 0; i < noSpaces.length; i++) {
     if (typeof noSpaces[i] == "string") {
-      arr.push(noSpaces[i][0]);
+      arr.push(noSpaces[i][0].trim());
     } else {
       continue;
     }
