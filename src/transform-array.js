6581eb46c5cb01a103313f73c4d7590def8d58cf
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-11 23:49:06 +0300

    fix: array task test issue fix

--- src/transform-array.js
+++ src/transform-array.js
@@ -7,4 +7,27 @@ module.exports = function transform(arr) {
   if (arr.length == 0) {
     return arr;
   }
+
+  let newArr = [];
+
+  for (i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == "number") {
+      newArr.push(arr[i]);
+    }
+    if (arr[i] == "--discard-next") {
+      i++;
+      continue;
+    }
+    if (arr[i] == "--discard-prev") {
+      newArr.pop(arr[i - 1]);
+      continue;
+    }
+    if (arr[i] == "--double-next") {
+      console.log("--double-next");
+    }
+    if (arr[i] == "--double-prev") {
+      console.log("--double-prev");
+    }
+  }
+  return newArr;
 };
