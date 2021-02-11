444193302c538ffd45925b892d8da2e70e13e964
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-11 21:48:54 +0300

    fix: task issue fix

--- src/what-season.js
+++ src/what-season.js
@@ -1,7 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if ((date = "undefined")) {
+  if (arguments.length == 0) {
     return "Unable to determine the time of year!";
   }
   let month = date.getMonth();
