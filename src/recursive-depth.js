249db8db49f3bf15452a8d9cf934f3235fb32fef
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-13 20:05:28 +0300

    fix: ADcalculator test issue fix

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,13 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    // my code here
+    return (
+      1 +
+      (Array.isArray(arr)
+        ? arr.reduce(function (max, item) {
+            return Math.max(max, calculateDepth(item));
+          }, 0)
+        : -1)
+    );
   }
 };
