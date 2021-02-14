a2b9a080cab36246276f12c3c613b4f57173e7cc
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-14 12:33:09 +0300

    fix: ERepeater code add

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,3 +1,24 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {};
+module.exports = function repeater(str, options) {
+  let firstLevelArray = [];
+  let secondLevelArray = [];
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    firstLevelArray.push(options.addition);
+  }
+
+  let firstLevelString = str + firstLevelArray.join(options.additionSeparator);
+
+  console.log(firstLevelString);
+
+  for (let j = 0; j < options.repeatTimes; j++) {
+    secondLevelArray.push(firstLevelString);
+  }
+
+  console.log(secondLevelArray);
+
+  let secondLevelString = secondLevelArray.join(options.separator);
+
+  console.log(secondLevelString);
+};
