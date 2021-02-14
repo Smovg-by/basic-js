c401ce11e6ee27805c423c07ec4779e44e10ba30
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-14 17:56:09 +0300

    fix: ER task test issue fix

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,11 +2,35 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   str = String(str);
+  options.addition = String(options.addition);
   let firstLevelArray = [];
   let secondLevelArray = [];
 
+  if (typeof options.repeatTimes !== "number") {
+    options.repeatTimes = 1;
+  }
+  if (typeof options.additionRepeatTimes !== "number") {
+    options.additionRepeatTimes = 1;
+  }
+
+  if (options.addition == "undefined") {
+    options.addition = "";
+  }
+
+  //   if (options.addition == null) {
+  //     options.addition = "null";
+  //   }
+
+  if (options.separator == undefined) {
+    options.separator = "+";
+  }
+
+  if (options.additionSeparator == undefined) {
+    options.additionSeparator = "|";
+  }
+
   for (let i = 0; i < options.additionRepeatTimes; i++) {
-    firstLevelArray.push(options.addition);
+    firstLevelArray.push(String(options.addition));
   }
 
   let firstLevelString = str + firstLevelArray.join(options.additionSeparator);
