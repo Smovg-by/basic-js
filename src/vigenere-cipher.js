68d1c648468aa7225ef1c3ce2859bdc6a7e33be8
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-14 20:47:57 +0300

    init: VCM task test issue fix

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,8 +1,59 @@
-const CustomError = require("../extensions/custom-error");
+// const CustomError = require("../extensions/custom-error");
 
-class VigenereCipheringMachine {
-  encrypt() {}
-  decrypt() {}
+// class VigenereCipheringMachine {
+//   encrypt() {
+
+//   }
+//   decrypt() {}
+// }
+
+// module.exports = VigenereCipheringMachine;
+
+function encrypt(message, key) {
+  let alphabetArray = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "q",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    " ",
+    "!",
+    "?",
+  ];
+
+  let messageArray = [];
+  let keyArray = [];
+
+  for (let i = 0; i < message.length; i++) {
+    messageArray.push(alphabetArray.indexOf(message[i]));
+  }
+
+  for (let j = 0; j < key.length; j++) {
+    keyArray.push(alphabetArray.indexOf(key[j]));
+  }
+
+  console.log(messageArray);
+
+  console.log(keyArray);
 }
-
-module.exports = VigenereCipheringMachine;
