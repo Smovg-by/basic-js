81f657c2af78388ef1849ba8f54b3253b00b7b06
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-19 21:32:58 +0300

    fix: VC task test  issue fix

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,48 +1,99 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// class VigenereCipheringMachine {
-//   encrypt() {
-
-//   }
-//   decrypt() {}
-// }
-
-// module.exports = VigenereCipheringMachine;
-
-function encrypt(message, key) {
-  let messageArray = [];
-  let keyArray = [];
-  let tempArray = [];
-  let encryptArray = [];
-  let encryptMessage;
-
-  messageArray = message.toUpperCase().split("");
-
-  keyArray = key.toUpperCase().split("");
-
-  for (i = 0, j = 0; i < messageArray.length; i++) {
-    if (
-      messageArray[i].charCodeAt(0) >= 65 &&
-      messageArray[i].charCodeAt(0) <= 90
-    ) {
-      tempArray.push(
-        messageArray[i].charCodeAt(0) +
-          keyArray[j % keyArray.length].charCodeAt(0) -
-          130
-      );
-      j++;
+class VigenereCipheringMachine {
+  constructor(modification = true) {
+    if (!modification) {
+      this.typeOfMachine = false;
     } else {
-      tempArray.push(messageArray[i]);
+      this.typeOfMachine = true;
     }
   }
 
-  for (let k = 0; k < tempArray.length; k++) {
-    if (typeof tempArray[k] == "number") {
-      encryptArray.push(String.fromCharCode((tempArray[k] % 26) + 65));
+  encrypt(message, key) {
+    let messageArray = [];
+    let keyArray = [];
+    let tempArray = [];
+    let encryptArray = [];
+    let encryptMessage;
+    if (message == undefined || key == undefined) {
+      throw Error;
+    }
+    messageArray = message.toUpperCase().split("");
+
+    keyArray = key.toUpperCase().split("");
+
+    for (i = 0, j = 0; i < messageArray.length; i++) {
+      if (
+        messageArray[i].charCodeAt(0) >= 65 &&
+        messageArray[i].charCodeAt(0) <= 90
+      ) {
+        tempArray.push(
+          messageArray[i].charCodeAt(0) +
+            keyArray[j % keyArray.length].charCodeAt(0) -
+            130
+        );
+        j++;
+      } else {
+        tempArray.push(messageArray[i]);
+      }
+    }
+
+    for (let k = 0; k < tempArray.length; k++) {
+      if (typeof tempArray[k] == "number") {
+        encryptArray.push(String.fromCharCode((tempArray[k] % 26) + 65));
+      } else {
+        encryptArray.push(tempArray[k]);
+      }
+    }
+    if (this.typeOfMachine) {
+      return encryptArray.join("");
     } else {
-      encryptArray.push(tempArray[k]);
+      return encryptArray.reverse().join("");
     }
   }
-  encryptMessage = encryptArray.join("");
-  return encryptMessage;
+
+  decrypt(encryptedMessage, key) {
+    let decryptArray = [];
+    let decryptKey = [];
+    let tempArray = [];
+    let finalArray = [];
+    let decryptedMessage;
+    if (encryptedMessage == undefined || key == undefined) {
+      throw Error;
+    }
+    decryptArray = encryptedMessage.toUpperCase().split("");
+
+    decryptKey = key.toUpperCase().split("");
+
+    for (i = 0, j = 0; i < decryptArray.length; i++) {
+      if (
+        decryptArray[i].charCodeAt(0) >= 65 &&
+        decryptArray[i].charCodeAt(0) <= 90
+      ) {
+        tempArray.push(
+          decryptArray[i].charCodeAt(0) -
+            decryptKey[j % decryptKey.length].charCodeAt(0)
+        );
+        j++;
+      } else {
+        tempArray.push(decryptArray[i]);
+      }
+    }
+    for (let k = 0; k < tempArray.length; k++) {
+      if (tempArray[k] < 0 && typeof tempArray[k] == "number") {
+        finalArray.push(String.fromCharCode(tempArray[k] + 26 + 65));
+      } else if (tempArray[k] >= 0 && typeof tempArray[k] == "number") {
+        finalArray.push(String.fromCharCode((tempArray[k] % 26) + 65));
+      } else {
+        finalArray.push(tempArray[k]);
+      }
+    }
+    if (this.typeOfMachine) {
+      return finalArray.join("");
+    } else {
+      return finalArray.reverse().join("");
+    }
+  }
 }
+
+module.exports = VigenereCipheringMachine;
